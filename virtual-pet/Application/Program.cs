88c15fe4bfd6038064c12fc401efd7ebc4e286c5
Karxem <tippjoshua@gmx.de>
2024-02-26 20:58:08 +0100

    feat: add option menu and first test options

--- virtual-pet/Application/Program.cs
+++ virtual-pet/Application/Program.cs
@@ -1,22 +1,59 @@
-﻿using virtual_pet.Base;
+﻿using virtual_pet.Core.Entities.Common;
 using virtual_pet.Core.Managers;
-using virtual_pet.Pets;
+using virtual_pet.Core.Models;
+using virtual_pet.Core.Utils;
 
-public class VirtualPets
+namespace virtual_pet.Application
 {
-    public static void Main()
+    public class VirtualPets
     {
-        PetManager petManager = new PetManager();
+        private static PetManager petManager = new PetManager();
 
-        var pets = petManager.GetPets();
-        Console.WriteLine("Your Pets:");
-        Console.WriteLine("-------------------------------------------------------");
-        foreach (var pet in pets)
+        static void Main()
         {
-            Console.WriteLine($"Name: {pet.Name}, Energy: {pet.Energy}, Hunger: {pet.Hunger}, Thirst: {pet.Thirst}");
-        }
+            List<string> menuItems = new List<string>
+            {
+            "Show Pet Overview",
+            "Fill all pet stats",
+            "Option 3",
+            "Exit"
+            };
+            ConsoleMenu consoleMenu = new ConsoleMenu(menuItems);
 
-        Console.ReadLine();
+            int selectedIndex = consoleMenu.ShowMenu();
+            var pets = petManager.GetPets();
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    Console.WriteLine("Your Pets:");
+                    Console.WriteLine("-------------------------------------------------------");
+                    foreach (var pet in pets)
+                    {
+                        Console.WriteLine($"Name: {pet.Name}, Energy: {pet.Energy}, Hunger: {pet.Hunger}, Thirst: {pet.Thirst}");
+                    }
+                    break;
+                case 1:
+                    foreach (var pet in pets)
+                    {
+                        var petInstance = petManager.LoadPet(pet.Name);
+                        petInstance.Eat(100);
+                        petInstance.Drink(100);
+                        petInstance.Sleep(100);
+
+                        petManager.SavePet(petInstance);
+                        Console.WriteLine($"{pet.Name} is filled up again!");
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Option 3");
+                    break;
+                default:
+                    Console.WriteLine("An error occured");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
     }
 }
-
