e6f6e0a9ef043d95df64bbd1ebfcdf9fb868e82f
eRoZexotrek <erozexotre@gmail.com>
2024-02-27 00:16:43 +0100

    feat: add console render engine

---
+++ virtual-pet/Core/Render/Buffer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtual_pet.Core.Render
+{
+    public class Buffer
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public ConsoleColor BackgroundColor { get; set; }
+        public ConsoleColor ForegroundColor {  get; set; }
+
+        ConsoleColor DefaultBackgroundColor = ConsoleColor.Black;
+        ConsoleColor DefaultForegroundColor = ConsoleColor.White;
+
+        int line = 0;
+
+        Pixel[,] buffer = null;
+
+        public Buffer(int x, int y, int width, int height)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Width = width;
+            this.Height = height;
+
+            this.buffer = new Pixel[width, height];
+        }
+
+        public void Clear()
+        {
+            line = 0;
+            for (int i = 0; i < Width; i++)
+            {
+                for (int j = 0; j < Height; j++)
+                {
+                    this.buffer[i, j].Clear();
+                }
+            }
+        }
+
+        public void WriteLine(string str)
+        {
+            for (int i = 0; i < str.Length || i < Width; i++)
+            {
+                if (str[i] == '\n')
+                {
+                    line++;
+                    continue;
+                }
+                buffer[i, line].Character = str[i];
+                buffer[i, line].Background = BackgroundColor;
+                buffer[i, line].Foreground = ForegroundColor;
+            }
+            line++;
+        }
+
+        public void WriteLine(string str, ConsoleColor back, ConsoleColor fore)
+        {
+            for (int i = 0; i < str.Length || i < Width; i++)
+            {
+                if (str[i] == '\n')
+                {
+                    line++;
+                    continue;
+                }
+                buffer[i, line].Character = str[i];
+                buffer[i, line].Background = back;
+                buffer[i, line].Foreground = fore;
+            }
+            line++;
+        }
+
+        public void ResetColor()
+        {
+            this.BackgroundColor = DefaultBackgroundColor;
+            this.ForegroundColor = DefaultForegroundColor;
+        }
+
+        public Pixel this[int x, int y]
+        {
+            get
+            {
+                if (x < 0 || x > Width ||
+                    y < 0 || y > Height)
+                    return new Pixel();
+                return buffer[x, y];
+            }
+            set
+            {
+                if (x < 0 || x > Width ||
+                    y < 0 || y > Height)
+                    return;
+                buffer[x, y] = value;
+            }
+        }
+    }
+}
---
+++ virtual-pet/Core/Utils/ConsoleMenuBuffered.cs
@@ -0,0 +1,76 @@
+﻿using virtual_pet.Core.Render;
+namespace virtual_pet.Core.Utils
+{
+    public class ConsoleMenuBuffered
+    {
+        private List<string> menuItems;
+        private int selectedItemIndex;
+
+        private Render.Buffer buffer;
+        public ConsoleMenuBuffered(Render.Buffer buffer, List<string> items)
+        {
+            this.buffer = buffer;
+            menuItems = items;
+            selectedItemIndex = 0;
+        }
+
+        public int ShowMenu()
+        {
+            ConsoleKeyInfo key;
+            do
+            {
+                buffer.Clear();
+                DisplayHeader();
+                DisplayMenu();
+
+                key = Console.ReadKey();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        MoveSelectionUp();
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        MoveSelectionDown();
+                        break;
+                }
+
+            } while (key.Key != ConsoleKey.Enter);
+
+            return selectedItemIndex;
+        }
+
+        private void DisplayHeader()
+        {
+            buffer.WriteLine("Welcome to Virtual Pet");
+            buffer.WriteLine("----------------------------\n");
+        }
+
+        private void DisplayMenu()
+        {
+            for (int i = 0; i < menuItems.Count; i++)
+            {
+                if (i == selectedItemIndex)
+                {
+                    buffer.ForegroundColor = ConsoleColor.Black;
+                    buffer.BackgroundColor = ConsoleColor.White;
+                }
+
+                buffer.WriteLine(menuItems[i]);
+
+                buffer.ResetColor();
+            }
+        }
+
+        private void MoveSelectionUp()
+        {
+            selectedItemIndex = (selectedItemIndex - 1 + menuItems.Count) % menuItems.Count;
+        }
+
+        private void MoveSelectionDown()
+        {
+            selectedItemIndex = (selectedItemIndex + 1) % menuItems.Count;
+        }
+    }
+}
