04c5b05ba4e428075abf9b305616d759b77cdd3f
eRoZexotrek <erozexotre@gmail.com>
2024-02-27 23:49:12 +0100

    refactor: changed the render function, should have more performance now

--- virtual-pet/Core/Render/Renderer.cs
+++ virtual-pet/Core/Render/Renderer.cs
@@ -9,17 +9,18 @@ namespace virtual_pet.Core.Render
     public class Renderer
     {
 
-        
-        public static Buffer MainBuffer = new Buffer(0, 0, 32, 20);
-        public static Buffer PlayBuffer = new Buffer(0, 0, 32, 12);
-        public static Buffer MenuBuffer = new Buffer(0, 13, 32, 7);
+
+        public static Buffer MainBuffer = new Buffer(0, 0, 64, 32);
+        public static Buffer PlayBuffer = new Buffer(0, 0, 64, 26);
+        public static Buffer MenuBuffer = new Buffer(0, 26, 64, 8);
 
 
-        List<Buffer> buffers = new ();
+        public static List<Buffer> buffers = new();
 
 
 
-        public static void FillBuffer(Buffer buffer) {
+        public static void FillBuffer(Buffer buffer)
+        {
             for (int i = 0; i < buffer.Width; i++)
             {
                 for (int j = 0; j < buffer.Height; j++)
@@ -34,20 +35,57 @@ namespace virtual_pet.Core.Render
             Console.Clear();
             FillBuffer(PlayBuffer);
             FillBuffer(MenuBuffer);
-
+            foreach(Buffer b in buffers)
+            {
+                FillBuffer(b);
+            }
         }
 
-        public static void ClearBuffers() {
+        public static void ClearBuffers()
+        {
             PlayBuffer.Clear();
             MenuBuffer.Clear();
+            foreach (Buffer b in buffers)
+            {
+                b.Clear();
+            }
         }
 
-        public static void Render() {
+        /*
+         * performance increase
+         */
+        public static void Render()
+        {
+            StringBuilder builder = new StringBuilder();
+            FillBuffers();
+
+
+            for (int line = 0; line < MainBuffer.Height; line++)
+            {
+                for (int i = 0; i < MainBuffer.Width; i++)
+                {
+
+                    builder.Append(ColorCodes.GetColor(MainBuffer[i, line].Background, MainBuffer[i, line].Foreground) + MainBuffer[i, line].Character);
+                    //Console.BackgroundColor = MainBuffer[i, line].Background;
+                    //Console.ForegroundColor = MainBuffer[i, line].Foreground;
+                    //Console.Write(MainBuffer[i, line].Character);
+                }
+                builder.AppendLine();
+            }
+            Console.Clear();
+            Console.Write(builder);
+        }
+
+        /*
+         * Dont use, looks like Shit (flickers)
+         */
+        public static void RenderOld()
+        {
             FillBuffers();
             Console.Clear();
-            for(int line=0; line < MainBuffer.Height; line++)
+            for (int line = 0; line < MainBuffer.Height; line++)
             {
-                for(int i =0; i< MainBuffer.Width; i++)
+                for (int i = 0; i < MainBuffer.Width; i++)
                 {
                     Console.BackgroundColor = MainBuffer[i, line].Background;
                     Console.ForegroundColor = MainBuffer[i, line].Foreground;
