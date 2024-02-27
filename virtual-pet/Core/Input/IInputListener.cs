a96f68d5edbe5ce267c3bb194b1cdebaac07a516
eRoZexotrek <erozexotrek@gmail.com>
2024-02-27 10:39:57 +0100

    feat: contains classes for input handling and deleagtion

---
+++ virtual-pet/Core/Input/IInputListener.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtual_pet.Core.Input {
+    public interface IInputListener {
+        public void KeyPressed(ConsoleKeyInfo info);
+    }
+}
---
+++ virtual-pet/Core/Input/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtual_pet.Core.Input {
+    public class PlayerController {
+        public IInputListener? Listener { get; set; }
+
+
+        public void Tick() {
+            if(!Console.KeyAvailable) 
+                return;
+            ConsoleKeyInfo? key = null;
+            while (Console.KeyAvailable)
+            {
+                key = Console.ReadKey(true);
+            }
+            Listener?.KeyPressed(key.Value);
+        }
+
+
+        public ConsoleKeyInfo? FetchInput() {
+            ConsoleKeyInfo? key = null;
+            while (Console.KeyAvailable)
+            {
+                key = Console.ReadKey(true);
+            }
+            return key;
+        }
+    }
+}
