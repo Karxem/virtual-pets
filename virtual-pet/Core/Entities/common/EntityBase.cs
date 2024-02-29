3e83ff17debcd1ecaff1d760b9f93497d349382f
eRoZexotrek <erozexotrek@gmail.com>
2024-02-29 20:48:19 +0100

    feat: used to read user text input (not tested and not final)

---
+++ virtual-pet/Core/Entities/common/EntityBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using virtual_pet.Core.Render;
+using virtual_pet.Core.Input;
+
+namespace virtual_pet.Core.Entities.Common {
+    public abstract class EntityBase : IInputListener, IDisplayable {
+
+        public Render.Vector2 Position { get; set; } = new Render.Vector2();
+        public Render.Vector2 Direction { get; set; } = new Render.Vector2();
+
+        private OptionStrip optionStrip = new OptionStrip(
+            //new OptionStripItem()
+        );
+
+        public bool Alive { get; private set; }
+
+       
+
+
+        public void Display(Render.Buffer buffer) {
+            
+        }
+
+        public virtual void Tick() { }
+
+        public virtual bool HitTest() {  return false; }
+
+        public virtual void KeyPressed(ConsoleKeyInfo key) {}
+    }
+}
