30f9797b4381ff8b0ef2b041fcf57786f573bd84
Joshua Tippelt <joshua.tippelt@brainbits.net>
2024-03-04 14:29:03 +0100

    refactor: extend PetModel fields to implement Level and Experience

--- virtual-pet/Core/Models/StatModel.cs
+++ virtual-pet/Core/Models/StatModel.cs
@@ -4,52 +4,46 @@ namespace virtual_pet.Core.Models
 {
     internal class StatModel
     {
-        private double _Value;
-        private double _MinValue;
-        private double _MaxValue;
+        private double _value;
+        private double _minValue;
+        private double _maxValue;
 
         public StatModel(double minValue, double maxValue)
         {
-            _MinValue = minValue;
-            _MaxValue = maxValue;
-            _Value = Math.Min(_MaxValue, Math.Max(_MinValue, 0.0));
+            _minValue = minValue;
+            _maxValue = maxValue;
+            _value = Math.Min(_maxValue, Math.Max(_minValue, 0.0));
         }
 
         public double Value
         {
-            get { return _Value; }
-            set { _Value = Math.Min(_MaxValue, Math.Max(_MinValue, value)); }
+            get => _value;
+            set => _value = Math.Min(_maxValue, Math.Max(_minValue, value));
         }
 
         public double MinValue
         {
-            get { return _MinValue; }
-            set { _MinValue = value; }
+            get => _minValue;
+            set => _minValue = value;
         }
 
         public double MaxValue
         {
-            get { return _MaxValue; }
-            set { _MaxValue = value; }
+            get => _maxValue;
+            set => _maxValue = value;
         }
 
         public void SetRange(double minValue, double maxValue)
         {
-            _MinValue = minValue;
-            _MaxValue = maxValue;
-            Value = _Value;
+            _minValue = minValue;
+            _maxValue = maxValue;
+            Value = _value;
         }
 
-        public static implicit operator double(StatModel stat) { return stat.Value; }
+        public static implicit operator double(StatModel stat) => stat.Value;
 
-        public static double operator +(StatModel stat, double value)
-        {
-            return stat.Value + value;
-        }
+        public static double operator +(StatModel stat, double value) => stat.Value + value;
 
-        public static double operator -(StatModel stat, double value)
-        {
-            return stat.Value - value;
-        }
+        public static double operator -(StatModel stat, double value) => stat.Value - value;
     }
 }
