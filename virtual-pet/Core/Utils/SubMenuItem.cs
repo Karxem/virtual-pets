b6e8e4d944cfa5e6384b5c0b6c09ec716b254f06
eRoZexotrek <erozexotrek@gmail.com>
2024-03-04 15:39:47 +0100

    feat: MenuItemBase and Children

---
+++ virtual-pet/Core/Utils/SubMenuItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtual_pet.Core.Utils {
+    public class SubMenuItem : MenuItemBase{
+
+        
+        public ConsoleMenu Menu { get; private set; }
+
+        public SubMenuItem(int id, string text ) : base(id, text) { }
+
+    }
+}
