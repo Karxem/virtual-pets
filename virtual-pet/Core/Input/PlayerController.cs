ef0163a48b62cb0536ec7d82a173494737a9fb79
eRoZexotrek <erozexotrek@gmail.com>
2024-03-04 15:43:28 +0100

    refactor: add bool PassAll to IInputListener

--- virtual-pet/Core/Input/PlayerController.cs
+++ virtual-pet/Core/Input/PlayerController.cs
@@ -8,11 +8,25 @@ namespace virtual_pet.Core.Input {
     public class PlayerController {
         public IInputListener? Listener { get; set; }
 
+        public bool PassAll { get; set; } = false;
+
 
         public void Tick() {
             if(!Console.KeyAvailable) 
                 return;
+            
             ConsoleKeyInfo? key = null;
+            bool passAll = Listener?.RequieresPassAll() != null ? Listener!.RequieresPassAll() : false;
+            //Required for Text Input 
+            if (passAll)
+            {
+                while (Console.KeyAvailable)
+                {
+                    key = Console.ReadKey(true);
+                    Listener?.KeyPressed(key.Value);
+                }
+                return;
+            }
             while (Console.KeyAvailable)
             {
                 key = Console.ReadKey(true);
