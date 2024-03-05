9349a18af6360ae5c05be893e732a15cabc8fefe
eRoZexotrek <erozexotrek@gmail.com>
2024-03-05 14:41:55 +0100

    feat: Engine class for gamestates and moved stuff out of Main

--- virtual-pet/Application/VirtualPet.cs
+++ virtual-pet/Application/VirtualPet.cs
@@ -5,112 +5,48 @@ using virtual_pet.Core.Utils;
 using virtual_pet.Core.Render;
 using virtual_pet.Core.Input;
 using virtual_pet.Core;
-using System.Security.Cryptography.X509Certificates;
-using System.Reflection.Metadata.Ecma335;
 
 namespace virtual_pet.Application {
     public class VirtualPets {
-        static PlayerController controller = new PlayerController();
+        
         static List<string> menuItems = new List<string>
             {
             "Show Pet Overview",
             "Fill all pet stats",
             "Add a pet",
             "Exit",
-            "5",
-            "6",
-            "7",
-            "8"
             };
-        static ConsoleMenu consoleMenu = new ConsoleMenu( onItemSelected, menuItems);
+       // static ConsoleMenu consoleMenu = new ConsoleMenu( onItemSelected, menuItems);
 
-        static List<string> mainMenuItems = new List<string>
-            {
-            "Change Interface Size",
-            "Close Game"
-            };
-        static ConsoleMenu mainMenu = new ConsoleMenu( onMainItemSelected, mainMenuItems);
-
-
-        static List<MenuItemBase> debugMenuItems = new List<MenuItemBase>
-            {
-            
-            };
-        static ConsoleMenu debugMenu = new ConsoleMenu( onDebugItemSelected, debugMenuItems);
-
-        static TextInput debugTextInput;//= TextInput.GetTextNumberInput(onDebugTextInput);
-
-        static Stack<ConsoleMenu> menus = new Stack<ConsoleMenu>();
-        static Stack<IInputListener> inputListeners = new Stack<IInputListener>();
-
-        static ConsoleMenu currentMenu ;
-        static OptionStrip? currentOtionStrip;
 
         static PetManager petManager = new PetManager();
         static List<PetModel> pets = new List<PetModel>();
-        static List<IDisplayable> displayables = new List<IDisplayable>();
-        static DebugPrint debugPrint = new DebugPrint();
+        
         static MainInputListener mainListener = new MainInputListener();
 
         static bool running = true;
 
-        public static void PassInput(IInputListener listener) {
-            if(controller.Listener != null)
-                controller.Listener.IsActive = false;
-            inputListeners.Push(controller.Listener);
-            controller.Listener = listener;
-            listener.IsActive = true;
-            currentOtionStrip = listener.GetOptionStrip();
-        }
 
-        public static void PopInput() {
-            controller.Listener.IsActive = false;
-            controller.Listener = inputListeners.Pop();
-            controller.Listener.IsActive = true;
-            currentOtionStrip = controller.Listener?.GetOptionStrip();
-        }
-
-        public static void OpenMenu(ConsoleMenu consoleMenu) {
-            menus.Push(currentMenu);
-            currentMenu = consoleMenu;
-            PassInput(consoleMenu);
-        }
-
-        public static void CloseMenu() {
-            currentMenu = menus.Pop();
-            PopInput();
-        }
 
 
         static void Main() {
 
-            inputListeners.Push(new MainInputListener());
+            //inputListeners.Push(new MainInputListener());
             Console.CursorVisible = false;
-            debugMenu.CreateClickItem("Debug Color Print");
-            debugTextInput = debugMenu.CreateTextInputItem(onDebugTextInput, "Debug Text Input Item").Input;
+            //debugMenu.CreateClickItem("Debug Color Print");
+            //debugTextInput = debugMenu.CreateTextInputItem(onDebugTextInput, "Debug Text Input Item").Input;
 
-
+            Engine.Init();
             //int selectedIndex = consoleMenu.ShowMenu();
             pets = petManager.GetPets();
-            PassInput(mainListener);
+            //PassInput(mainListener);
             running = true;
             while (running)
             {
-                //clear buffers
                 Renderer.ClearBuffers();
-                //fetch input
-                controller.Tick();
-                //Renderer.FitToScreen();
-                //
-                foreach (IDisplayable d in displayables)
-                {
-                    d.Display(Renderer.PlayBuffer);
-                }
-                currentMenu?.Display(Renderer.MenuBuffer);
-                currentOtionStrip?.Display(Renderer.OptionStripBuffer);
-                //render the scene
+                Engine.Tick();
                 Renderer.Render();
-                Thread.Sleep(30);
+                Thread.Sleep(16);
             }
 
             Console.WriteLine("Exiting...");
@@ -150,7 +86,7 @@ namespace virtual_pet.Application {
                     string petName = Console.ReadLine();
 
                     PetBase newPet = new Lenora();
-                    newPet.Name = petName;
+                    newPet.PetName = petName;
                     newPet.FillAll();
                     petManager.SavePet(newPet);
 
@@ -166,100 +102,6 @@ namespace virtual_pet.Application {
 
             Renderer.MainBuffer.WriteLine("\nPress Enter to go back to the main menu...");
             Console.ReadLine();
-        }
-
-        public static void onDebugItemSelected(object sender, int selectedIndex) {
-            switch (selectedIndex)
-            {
-                case -1:
-                    CloseMenu();
-                    break;
-                case 0:
-                    if(!displayables.Contains(debugPrint))
-                        displayables.Add(debugPrint);
-                    else
-                        displayables.Remove(debugPrint);
-                    break;
-                case 1:
-                    PassInput(debugTextInput);
-                    break;
-                default: break;
-            }
-            
-        }
-
-        public static void onMainItemSelected(object sender, int selectedIndex) {
-            switch (selectedIndex)
-            {
-                case -1:
-                    CloseMenu();
-                    break;
-                case 0:
-                    break;
-                case 1:
-                    running = false;return;
-                default:
-                    break;
-            }
-        }
-
-
-        public class ChangeSizeListener : IInputListener {
-
-            public bool IsActive { get; set; }
-
-            OptionStrip optionStrip = new OptionStrip();
-
-            public void KeyPressed(ConsoleKeyInfo key) {
-                switch(key.Key){
-                    case ConsoleKey.DownArrow:
-                    {
-                        
-
-                    }break;
-                    case ConsoleKey.UpArrow:
-                    {
-
-                    }
-                    break;
-                    case ConsoleKey.RightArrow:
-                    {
-
-                    }
-                    break;
-                    case ConsoleKey.LeftArrow:
-                    {
-
-                    }
-                    break;
-                    default: break;
-                }
-            }
-
-            public OptionStrip? GetOtptionStrip() => optionStrip;
-        }
-
-        public static void onDebugTextInput(object sender, string text) {
-            PopInput();
-        }
-        
-        public class MainInputListener : IInputListener {
-
-            public bool IsActive { get; set; }
-            public void KeyPressed(ConsoleKeyInfo key) {
-                switch (key.Key)
-                {
-                    case ConsoleKey.Escape:
-                        OpenMenu(mainMenu);
-                        break;
-                    case ConsoleKey.F2:
-                        OpenMenu(debugMenu);
-                        break;
-                    default:
-                        break;
-
-                }
-            }
-        }
+        }        
     }
 }
---
+++ virtual-pet/Core/Engine.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using virtual_pet.Core.Input;
+using virtual_pet.Core.Render;
+using virtual_pet.Core.Utils;
+using virtual_pet.Core.Menu;
+
+namespace virtual_pet.Core {
+    public class Engine {
+
+        private static PlayerController controller = new PlayerController();
+
+        private static Stack<IInputListener> inputListeners = new Stack<IInputListener>();
+
+        private static List<IDisplayable> displayables = new List<IDisplayable>();
+
+        private static Stack<ConsoleMenu> menus = new Stack<ConsoleMenu>();
+
+        private static ConsoleMenu currentMenu;
+
+        private static OptionStrip? currentOtionStrip;
+
+        public static bool IsRunning { get; private set;  } = false;
+
+
+        public static void Init() {
+            controller.Listener = new MainInputListener();
+            inputListeners.Push(controller.Listener);
+            controller.Listener.IsActive = true;
+            IsRunning = true;
+        }
+
+        public static void Tick() {
+            controller.Tick();
+            foreach (IDisplayable d in displayables)
+            {
+                d.Display(Renderer.PlayBuffer);
+            }
+            currentMenu?.Display(Renderer.MenuBuffer);
+            currentOtionStrip?.Display(Renderer.OptionStripBuffer);
+        }
+
+        public static void AddDisplayable(IDisplayable displayable) {
+            displayables.Add(displayable);
+        }
+
+        public static bool ContainsDisplayable(IDisplayable dislpayable) {
+            return displayables.Contains(dislpayable);
+        }
+
+        public static void RemoveDisplayable(IDisplayable displayable) {
+            displayables.Remove(displayable);
+        }
+        
+
+        public static void PassInput(IInputListener listener) {
+            if (controller.Listener != null)
+                controller.Listener.IsActive = false;
+            inputListeners.Push(controller.Listener);
+            controller.Listener = listener;
+            listener.IsActive = true;
+            currentOtionStrip = listener.GetOptionStrip();
+        }
+
+        public static void PopInput() {
+            controller.Listener.IsActive = false;
+            controller.Listener = inputListeners.Pop();
+            controller.Listener.IsActive = true;
+            currentOtionStrip = controller.Listener?.GetOptionStrip();
+        }
+
+        public static void OpenMenu(ConsoleMenu consoleMenu) {
+            menus.Push(currentMenu);
+            currentMenu = consoleMenu;
+            PassInput(consoleMenu);
+        }
+
+        public static void CloseMenu() {
+            currentMenu = menus.Pop();
+            PopInput();
+        }
+
+    }
+    public class MainInputListener : IInputListener {
+
+        public bool IsActive { get; set; }
+        public void KeyPressed(ConsoleKeyInfo key) {
+            switch (key.Key)
+            {
+                case ConsoleKey.Escape:
+                    Engine.OpenMenu(Menus.MainMenu.Menu);
+                    break;
+                case ConsoleKey.F2:
+                    Engine.OpenMenu(Menus.DebugMenu.Menu);
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
