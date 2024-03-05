53ada4af13cf8297b6fb337347fc0d72c8eb979b
eRoZexotrek <erozexotrek@gmail.com>
2024-03-05 14:37:01 +0100

    feat: designated folder for commonly used menus

---
+++ virtual-pet/Core/Menu/Menus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtual_pet.Core.Menu {
+    /**
+     * container class for menu declarations
+     */
+    public class Menus {
+        public static MainMenu MainMenu = new MainMenu();
+        public static DebugMenu DebugMenu = new DebugMenu();
+    }
+}
