cebc4c7e6ba9e458bd7f433e56f64e9ff68ab0d3
Joshua Tippelt <joshua.tippelt@brainbits.net>
2024-03-06 16:08:05 +0100

    feat: create new pet type Flololo

---
+++ virtual-pet/Core/Entities/Pets/Flololo.cs
@@ -0,0 +1,14 @@
+﻿using virtual_pet.Core.Entities.Common;
+using virtual_pet.Core.Models;
+
+namespace virtual_pet.Core.Entities.Pets
+{
+    internal class Flololo : PetBase
+    {
+        public override string GetPetType()
+        {
+            return "Flololo";
+        }
+    }
+}
+
