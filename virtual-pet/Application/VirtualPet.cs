466e4e6a3c6e0cbdc44cbed22621f45bcb3ed7d5
Joshua Tippelt <joshua.tippelt@brainbits.net>
2024-03-08 23:11:43 +0100

    refactor(Application): make each option its on function-

--- virtual-pet/Application/VirtualPet.cs
+++ virtual-pet/Application/VirtualPet.cs
@@ -1,28 +1,30 @@
-﻿using virtual_pet.Core;
-using virtual_pet.Core.Entities.common;
+﻿using System;
+using System.Collections.Generic;
 using virtual_pet.Core.Entities.Common;
 using virtual_pet.Core.Levels;
 using virtual_pet.Core.Managers;
+using virtual_pet.Core.Models;
 using virtual_pet.Core.Utils;
 
 namespace virtual_pet.Application
 {
     public class VirtualPets
     {
-        static void Main()
+        private static readonly PetManager petManager = new PetManager();
+
+        public static void Main()
         {
             List<string> menuItems = new List<string>
             {
-            "Show Pet Overview",
-            "Fill all pet stats",
-            "Add a pet",
-            "Gain experience",
-            "Take a walk",
-            "Exit"
+                "Show Pet Overview",
+                "Fill all pet stats",
+                "Add a pet",
+                "Gain experience",
+                "Test first level",
+                "Exit"
             };
 
             ConsoleMenu consoleMenu = new ConsoleMenu(menuItems);
-            PetManager petManager = new PetManager();
 
             while (true)
             {
@@ -37,85 +39,116 @@ namespace virtual_pet.Application
                 switch (selectedIndex)
                 {
                     case 0:
-                        if (pets.Count == 0)
-                        {
-                            Console.Clear();
-                            Console.WriteLine("You dont have any Pets");
-                            Console.WriteLine("-------------------------------------------------------");
-                        }
-                        else
-                        {
-                        Console.WriteLine("Your Pets:");
-                        Console.WriteLine("-------------------------------------------------------");
-                        
-                        foreach (var pet in pets)
-                        {
-                            Console.WriteLine($"⎡ Name: {pet.Name}, Type: {pet.Type}, Level: {pet.Level}");
-                            Console.WriteLine($"⎢ Energy: {pet.Energy}, Attack: {pet.Attack}, Defense: {pet.Defense}");
-                            Console.WriteLine($"⎣ Health: { pet.Health}, Hunger: { pet.Hunger}, Thirst: { pet.Thirst}");
-                        }
-                        }
-
+                        ShowPetOverview(pets);
                         break;
                     case 1:
-                        if (pets.Count == 0)
-                        {
-                            Console.Clear();
-                            Console.WriteLine("You dont have any Pets");
-                            Console.WriteLine("-------------------------------------------------------");
-                        }
-                        foreach (var pet in pets)
-                        {
-                            var petInstance = petManager.LoadPet(pet.Name);
-                            petInstance.FillAll();
-
-                            petManager.SavePet(petInstance);
-
-                            Console.WriteLine($"{pet.Name} is filled up again!");
-                        }
+                        FillAllPetStats(pets);
                         break;
                     case 2:
-                        Console.Clear ();
-
-                        Console.Write("Name your new Pet: ");
-                        string petName = Console.ReadLine();
-
-                        PetBase newPet = new Lenora();
-                        newPet.PetName = petName;
-                        newPet.FillAll();
-                        petManager.SavePet(newPet);
-
-                        Console.WriteLine($"Your new pet {petName} was added");
+                        AddNewPet();
                         break;
                     case 3:
-                        Console.Clear();
-
-                        foreach (var pet in pets)
-                        {
-                            var petInstance = petManager.LoadPet(pet.Name);
-                            petInstance.GainExperience(100.0);
-
-                            petManager.SavePet(petInstance);
-                        }
+                        GainExperience(pets);
                         break;
                     case 4:
-                        Console.Clear();
-
                         LevelBase level = new TestLevel();
-
                         level.StartFight(pets[0].Name);
                         break;
                     case 5:
                         return;
                     default:
-                        Console.WriteLine("An error occured");
+                        Console.WriteLine("An error occurred");
                         break;
                 }
 
-                Console.WriteLine("\nPress Enter to go back to the main menu...");
+                Console.WriteLine("Press Enter to go back to the main menu...");
                 Console.ReadLine();
             }
+        }
 
+        private static void ShowPetOverview(List<PetModel> pets)
+        {
+            Console.Clear();
+            Console.WriteLine(SendHeaderText(pets.Count));
+            Console.WriteLine("-------------------------------------------------------");
+
+            foreach (var pet in pets)
+            {
+                Console.WriteLine($"⎡ Name: {pet.Name}, Type: {pet.Type}, Level: {pet.Level}");
+                Console.WriteLine($"⎢ Energy: {pet.Energy}, Attack: {pet.Attack}, Defense: {pet.Defense}");
+                Console.WriteLine($"⎣ Health: {pet.Health}, Hunger: {pet.Hunger}, Thirst: {pet.Thirst}\n");
+            }
+        }
+
+        private static void FillAllPetStats(List<PetModel> pets)
+        {
+            Console.Clear();
+            foreach (var pet in pets)
+            {
+                var petInstance = petManager.LoadPet(pet.Name);
+                petInstance.FillBasicNeeds();
+                petManager.SavePet(petInstance);
+                Console.WriteLine($"{pet.Name} is filled up again!");
+            }
+        }
+
+        private static void AddNewPet()
+        {
+            Console.Clear();
+            Console.Write("Name your new pet: ");
+            string petName = Console.ReadLine();
+
+            // Get a dynamically built list of pet types
+            List<string> petTypes = PetBase.GetPetTypes();
+
+            // Display the list of pet types
+            Console.WriteLine("Choose a pet type:");
+            for (int i = 0; i < petTypes.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {petTypes[i]}");
+            }
+
+            // Read user input for pet type index
+            int typeIndex = Convert.ToInt32(Console.ReadLine());
+
+            // Validate user input and create a new pet instance
+            if (typeIndex < 1 || typeIndex > petTypes.Count)
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid pet type selection!");
+                return;
+            }
+
+            string petType = petTypes[typeIndex - 1];
+            PetBase newPet = petManager.CreateNewPetInstance(petName, petType);
+
+            if (newPet == null)
+            {
+                return;
+            }
+
+            petManager.SavePet(newPet);
+            Console.Clear();
+            Console.WriteLine($"Your new pet {petName} of type {petType} was added");
+        }
+
+
+        private static void GainExperience(List<PetModel> pets)
+        {
+            Console.Clear();
+            foreach(var pet in pets)
+            {
+                PetBase activePet = petManager.LoadPet(pet.Name);
+                Random random = new Random();
+                activePet.GainExperience(random.Next(2, 10));
+
+                petManager.SavePet(activePet);
+            }
+        }
+
+        private static string SendHeaderText(int count)
+        {
+            return count == 0 ? "You don't have any pets" : "Your current pets";
         }
     }
 }
