e41b5d70aae42167ace7d624152b2a2d62cc5965
Karxem <tippjoshua@gmx.de>
2024-03-10 13:48:43 +0100

    feat: create PetConverter for proper deserialization of PetBase objects

---
+++ virtual-pet/Core/Utils/PetConverter.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Reflection;
+using virtual_pet.Core.Entities.Common;
+
+internal class PetConverter : JsonConverter<PetBase>
+{
+    public override PetBase ReadJson(JsonReader reader, Type objectType, PetBase existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        JObject jsonObject = JObject.Load(reader);
+
+        string typeName = jsonObject["Type"]?.ToString();
+
+        if (typeName == null)
+        {
+            Console.WriteLine("Pet type is missing in JSON.");
+            return null;
+        }
+
+        // Use reflection to create an instance of the concrete type based on the "Type" property
+        Type type = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(t => t.Name == typeName);
+
+        if (type == null || !typeof(PetBase).IsAssignableFrom(type))
+        {
+            Console.WriteLine($"Invalid or unknown type: {typeName}");
+            return null;
+        }
+
+        PetBase pet = (PetBase)Activator.CreateInstance(type);
+        serializer.Populate(jsonObject.CreateReader(), pet);
+        return pet;
+    }
+
+    public override void WriteJson(JsonWriter writer, PetBase value, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+}
