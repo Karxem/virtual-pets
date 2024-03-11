e30fd2d0d895ae91d36d0856e7a10fbd4ff10427
Karxem <tippjoshua@gmx.de>
2024-03-11 01:18:25 +0100

    feat: create first item HealPotion

---
+++ virtual-pet/Core/Entities/Items/HealPotion.cs
@@ -0,0 +1,42 @@
+﻿using virtual_pet.Core.Entities.Common;
+using virtual_pet.Core.Managers;
+
+namespace virtual_pet.Core.Entities.Items
+{
+    internal class HealingPotion : ItemBase
+    {
+        private static readonly PetManager petManager = new PetManager();
+        public int HealingAmount { get; private set; }
+
+        public override string Name => "Healing Potion";
+
+        public override string Description => "Heals half of the HP points of a pet.";
+
+        public HealingPotion(int healingAmount)
+        {
+            HealingAmount = healingAmount;
+        }
+
+        public override void UseItem(PetBase targetPet)
+        {
+            ApplyEffect(targetPet);
+            RemoveItem(1);
+
+            Console.Clear();
+            Console.WriteLine($"{targetPet.Name} used {Name} and healed for {HealingAmount} health points.\n");
+        }
+
+        public override void DisplayItemInfo()
+        {
+            Console.WriteLine($"Item: {Name}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Healing Amount: {HealingAmount}");
+        }
+
+        public override void ApplyEffect(PetBase targetPet)
+        {
+            targetPet.Heal(HealingAmount);
+            petManager.SavePet(targetPet);
+        }
+    }
+}
