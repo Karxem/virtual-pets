0602af54ffe2c442f308512f1d6327fa1975d944
Karxem <tippjoshua@gmx.de>
2024-03-11 01:17:58 +0100

    feat: introduce ItemBase and implement base functions

---
+++ virtual-pet/Core/Entities/common/ItemBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using virtual_pet.Core.Entities.Common;
+
+namespace virtual_pet.Core.Entities.Items
+{
+    internal abstract class ItemBase
+    {
+        public abstract string Name { get; }
+        public abstract string Description { get; }
+        public int Count { get; set; }
+
+        public void AddItem(int amount) => Count += amount;
+        public void RemoveItem(int amount) => Count -= amount;
+
+        public abstract void UseItem(PetBase targetPet);
+        public abstract void DisplayItemInfo();
+        public abstract void ApplyEffect(PetBase targetPet);
+    }
+}
