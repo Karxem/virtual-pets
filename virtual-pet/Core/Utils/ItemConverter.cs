4ae85af5d8c09e99c9d47f5e3ef5db8ce3986d56
Karxem <tippjoshua@gmx.de>
2024-03-11 18:00:08 +0100

    feat: create ItemConverter to deserialize ItemBase objects

---
+++ virtual-pet/Core/Utils/ItemConverter.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using virtual_pet.Core.Entities.Items;
+
+namespace virtual_pet.Core.Utils
+{
+    internal class ItemConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(ItemBase));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject itemJson = JObject.Load(reader);
+
+            // Check for the presence of the "HealingAmount" property to determine the concrete type
+            if (itemJson["HealingAmount"] != null)
+            {
+                return itemJson.ToObject<HealingPotion>(serializer);
+            }
+
+            // Add more checks for other concrete types if needed
+
+            throw new JsonSerializationException("Unable to determine the concrete type of the item.");
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
