c816b9cb583d9e2259dc11595ffacb5e23834d55
Karxem <tippjoshua@gmx.de>
2024-03-11 21:14:03 +0100

    refactor: update TestLevel to match LevelBase

---
+++ virtual-pet/Core/Entities/Levels/TestLevel.cs
@@ -0,0 +1,34 @@
+﻿using virtual_pet.Core.Entities.Common.Level;
+using virtual_pet.Core.Entities.Items;
+using virtual_pet.Core.Manager;
+using virtual_pet.Core.Models;
+
+namespace virtual_pet.Core.Entities.Levels
+{
+    internal class TestLevel : LevelBase
+    {
+        private static readonly ShopManager shopManager = new ShopManager();
+        private Shop shop;
+
+        public override string Name => "Test Level";
+        public override BiomeType Biome => BiomeType.ForestPath;
+        public override bool HasCity => false;
+        public override bool HasShop => true;
+
+        public override void StartFight()
+        {
+            Console.WriteLine("Fight!");
+        }
+
+        public override void VisitShop()
+        {
+            Console.WriteLine("Shopping time!");
+
+            List<ItemBase> shopItems = new List<ItemBase>();
+            shopItems.Add(new HealingPotion(1));
+            shop = new ShopManager().CreateShop(shopItems);
+
+            shop.DisplayAvailableItems();
+        }
+    }
+}
