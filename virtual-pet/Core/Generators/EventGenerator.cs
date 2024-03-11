9923d3f15c4c3e6b4c5b8856b3e82eb15c2283c1
Karxem <tippjoshua@gmx.de>
2024-03-11 21:14:29 +0100

    feat: create EventGenerator to generate the next random event

---
+++ virtual-pet/Core/Generators/EventGenerator.cs
@@ -0,0 +1,19 @@
+﻿internal class EventGenerator
+{
+    private static readonly Random random = new Random();
+
+    public static LevelEvent GenerateRandomEvent()
+    {
+        Array events = Enum.GetValues(typeof(LevelEvent));
+
+        if (events == null || events.Length == 0)
+        {
+            Console.WriteLine("No events available.");
+            return LevelEvent.DefaultEvent; // Replace DefaultEvent with an appropriate default value
+        }
+
+        LevelEvent randomEvent = (LevelEvent)events.GetValue(random.Next(events.Length));
+
+        return randomEvent;
+    }
+}
