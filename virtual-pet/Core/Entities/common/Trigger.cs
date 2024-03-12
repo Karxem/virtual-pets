2668013cf7b2b0fb92d2ace90208115debd9eac6
eRoZexotrek <erozexotrek@gmail.com>
2024-03-12 10:45:16 +0100

    feat: collision and tilemap

---
+++ virtual-pet/Core/Entities/common/Trigger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtual_pet.Core.Entities.Common {
+    public class Trigger : ICollidable {
+        public TileMap Body { get; set; }
+
+        public string TriggerMessage = "";
+
+        public Trigger() { 
+            
+        }
+
+        public CollisionResult Colliding(ICollidable collidable) {
+            return CollisionResult.NoCollision;
+        }
+    }
+}
